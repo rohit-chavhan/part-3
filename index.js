679b0ed9e827b4c61db697c5de66a2aad7debb77
rohit-chavhan <rohitchavhan46@gmail.com>
2024-12-01 13:54:40 +0530

    updated git ignore file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(express.static('dist'));
 
 morgan.token('len', (req) => req.headers['content-length']);
 
