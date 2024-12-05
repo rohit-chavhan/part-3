e51cb315c087d6a24de9222aec03fd2f95856db7
rohit-chavhan <rohitchavhan46@gmail.com>
2024-12-05 13:31:50 +0530

    complete exercise 3.9-3.18

--- index.js
+++ index.js
@@ -1,114 +1,120 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('dist'));
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const app = express()
 
-const Contact = require('./models/phone');
+app.use(express.static('dist'))
+app.use(express.json())
+app.use(cors())
 
-morgan.token('len', (req) => req.headers['content-length']);
+const Contact = require('./models/phone')
+
+morgan.token('len', (req) => req.headers['content-length'])
 
 morgan.token('body', (req) =>
   req.method === 'POST' ? JSON.stringify(req.body) : ''
-);
+)
 
-morgan.token('status', (req, res) => res.statusCode);
+morgan.token('status', (req, res) => res.statusCode)
 
-app.use(morgan(':method :url :status :len :response-time ms :body'));
+app.use(morgan(':method :url :status :len :response-time ms :body'))
 
-let phoneBook = [
-  {
-    id: '1',
-    name: 'Arto Hellas',
-    number: '040-123456',
-  },
-  {
-    id: '2',
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-  },
-  {
-    id: '3',
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-  },
-  {
-    id: '4',
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-  },
-];
-
-app.get('/', (request, response) => response.send('<h1>Hello world </h1>'));
+app.get('/', (request, response) => response.send('<h1>Hello world </h1>'))
 
 app.get('/api/persons', (request, response) => {
-  response.json(phoneBook);
   Contact.find({}).then((contacts) => {
-    response.json(contacts);
-  });
-});
+    response.json(contacts)
+  })
+})
 
 app.get('/info', (request, response) => {
-  const totalNumber = phoneBook.length;
-  const time = new Date();
-  response.send(`
-  <p>phoneBook has info for ${totalNumber} people</p>
-  <br />
-  <p>${time}</p>
-    `);
-});
+  Contact.find({}).then((contacts) => {
+    const time = new Date()
+    response.send(`
+      <p>phoneBook has info for ${contacts.length} people</p>
+      <br />
+      <p>${time}</p>
+        `)
+  })
+})
 
-app.get('/api/persons/:id', (request, response) => {
-  Contact.findById(request.params.id).then((personContact) =>
-    response.json(personContact)
-  );
-});
+app.get('/api/persons/:id', (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((personContact) => {
+      if (personContact) {
+        response.json(personContact)
+      } else {
+        response.status(400).end()
+      }
+    })
+    .catch((error) => next(error))
+})
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = phoneBook.filter((contact) => contact.id !== id);
-  phoneBook = person;
-  response.status(204).end();
-});
+  console.log('deleted')
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
+  Contact.findByIdAndDelete(request.params.id)
+    .then((result) => response.status(204).end())
+    .catch((error) => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+  const body = request.body
+  console.log(body)
 
   if (!body.name) {
     return response.status(400).json({
       error: 'name missing',
-    });
+    })
   }
 
   if (!body.number) {
     return response.status(400).json({
       error: 'number missing',
-    });
+    })
   }
 
-  const same = phoneBook.filter((person) => person.name === body.name);
+  Contact.find({}).then((res) => {
+    let value = res.filter((obj) => obj.name === body.name)
+    console.log(value)
 
-  if (same.length > 0) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    });
+    if (value.length > 0) {
+      const person = {
+        name: value[0].name,
+        number: body.number,
+      }
+      Contact.findByIdAndUpdate(value[0].id, person, { new: true })
+        .then((updateContact) => response.json(updateContact))
+        .catch((error) => next(error))
+    } else {
+      const person = new Contact({
+        name: body.name,
+        number: body.number,
+      })
+
+      person.save().then((saveContact) => {
+        response.json(saveContact)
+      })
+    }
+  })
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown Endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message, error.name, 'ursrttd')
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
   }
+}
 
-  const person = new Contact({
-    name: body.name,
-    number: body.number,
-  });
+app.use(errorHandler)
 
-  person.save().then((saveContact) => {
-    console.log(typeof saveContact);
-    response.json(saveContact);
-  });
-});
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`port listening on ${PORT} port`);
-});
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`port listening on ${PORT} port`))
